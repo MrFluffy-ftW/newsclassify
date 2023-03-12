0f170472ccc7c9058335e7e30b3d320697a091fc
Anuj Shrestha <anujshrestha.as22@gmail.com>
2023-03-12 12:24:49 +0545

    new logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../images/Logoer.png";
+import logo from "../images/snap-news-logo2.png";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
